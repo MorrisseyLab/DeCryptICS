204e12ab6be9dff174341356d968062bcb18d014
Doran <doran.khamis@gmail.com>
2020-11-23 12:04:05 +0000

    cleaned up install instructions

--- bulk_block_gen.sh
+++ bulk_block_gen.sh
@@ -3,9 +3,10 @@
 # get block list as variable
 shopt -s nullglob
 #blocklist=(/full/path/to/block/folders/blocknames*/)
-blocklist=(/home/doran/Work/images/Leeds_May2019/splitbyKM/KM*/)
+#blocklist=(/home/doran/Work/images/Leeds_May2019/splitbyKM/KM*/)
 #blocklist=(/home/doran/Work/images/Serial_blocks_Oct2019/block*/)
 #blocklist=(/home/doran/Work/images/KRAS_study/block*/)
+blocklist=(/home/doran/Work/images/Leeds_May2019/splitbyKM/newbatch_23Nov20/KM*)
 
 for i in "${blocklist[@]}"
 do
--- bulk_block_run.sh
+++ bulk_block_run.sh
@@ -4,7 +4,7 @@
 shopt -s nullglob
 
 #blocklist=(/full/path/to/block/folders/block*/input_files.txt)
-blocklist=(/home/doran/Work/images/Leeds_May2019/splitbyKM/newbatch_16Sept20/KM*/input_files.txt)
+blocklist=(/home/doran/Work/images/Leeds_May2019/splitbyKM/newbatch_23Nov20/KM*/input_files.txt)
 #blocklist=(/home/doran/Work/images/Serial_blocks_Oct2019/block*/input_files.txt)
 #blocklist=(/home/doran/Work/images/KRAS_study/block*/input_files.txt)
 
